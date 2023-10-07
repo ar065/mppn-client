7832f846cc4f2184a429a049cf54e806c0f7d030
wolfy01 <wolfy3543@gmail.com>
2023-10-07 01:34:15 +0000

    a

---
+++ src/client/Client.ts
@@ -0,0 +1,42 @@
+import ClientOptions from "../types/ClientOptions";
+
+// TODO: Emit events, duh!
+export default class Client {
+    uri: string;
+    socket: WebSocket | null;
+
+    constructor(options: ClientOptions) {
+        this.uri = options.uri;
+
+        this.socket = null;
+    }
+
+    private parseMessages(data: string) {
+        try {
+            return JSON.parse(data);
+        } catch (error) {
+            return false;
+        }
+    }
+
+    private onopen() {}
+    private onclose() {}
+    private onerror() {}
+    private onmessage(event: MessageEvent) {
+        const messages = this.parseMessages(event.data);
+        if (messages) {
+            messages.forEach(message => {});
+        }
+    }
+
+    start() {
+        if (!this.socket) {
+            this.socket = new WebSocket(this.uri);
+            this.socket.binaryType = "arraybuffer";
+            this.socket.onopen = this.onopen;
+            this.socket.onclose = this.onclose;
+            this.socket.onerror = this.onerror;
+            this.socket.onmessage = this.onmessage;
+        }
+    }
+}
